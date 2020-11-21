6cf4e6703b35efced62aaae25efa68c7e1e7885c
lrodri14 <adolfoluis.rl@gmail.com>
2020-11-20 20:17:45 -0600

    Documented the Patients CSS Stylesheets and JS Code

--- patients/static/patients/js/add_patient.js
+++ patients/static/patients/js/add_patient.js
@@ -1,36 +1,66 @@
-//Checked
-var form = document.querySelector('form')
+/*This JS file contains all the variable definitions, async and sync functions, and event listeners for the template to
+  to perform correctly, the variables are divided into three sections, the backedUpData variables, the management form
+  variables and finally the clonedNodes variable.*/
+
+// #############################################Variables###############################################################
+
+// backedUpData variables
+var allergySelection
+
+// managementFormVariables
 var allergiesTotalForms = document.querySelector('.allergies_management_form > #id_form-TOTAL_FORMS')
 var antecedentsTotalForms = document.querySelector('.antecedents_management_form > #id_form-TOTAL_FORMS')
+
+// clonedNodes Variables
+var allergiesFormBlueprint = document.querySelector('.allergies-form .form-container').cloneNode(true)
+var antecedentsFormBlueprint = document.querySelector('.antecedents-form .form-container').cloneNode(true)
+
+var form = document.querySelector('form')
 var inputs = document.querySelectorAll('input')
 var generalInfoInputs = document.querySelectorAll('.general-info input, .general-info select')
 var button = document.querySelectorAll('button')
 var extraInfo = document.querySelector('.extra-info')
-var allergySelection
 var insuranceSelection = document.querySelector('.insurance-form select')
-var allergiesFormBlueprint = document.querySelector('.allergies-form .form-container').cloneNode(true)
-var antecedentsFormBlueprint = document.querySelector('.antecedents-form .form-container').cloneNode(true)
 var allergiesFormsContainer = document.querySelector('.allergies-form tbody')
 var antecedentsFormsContainer = document.querySelector('.antecedents-form tbody')
 var modal = document.querySelector('.modal')
 var modalContent = document.querySelector('.modal-content')
 var saveConfirmationModal = document.querySelector('.save-confirmation-modal')
-// Async Functions
+
+
+// #############################################Functions###############################################################
 
 async function elementAdditionFormAsync(url){
+    /*This async function is used to retrieve the form to add elements as Allergies or Insurance carriers from the ser
+      ver, and be able to display it in the modal, the only argument in accepts is an 'url', to where the GET request
+      will be done, the data received will be returned in JSON Format.*/
     const result = await fetch(url)
     const data = await result.json()
     return data
 }
 
 async function addElementAsync(url, method, csrfmiddlewaretoken, formData){
+    /*The addElementsAsync async function is used to add elements to the database asynchronously, it is used to add
+      elements as Allergies or Insurance Carriers, it takes four arguments we all retrieve from the form that fires
+      the submit event: 'url' to where the POST request will be done, the 'method' that we retrieve from the form.action
+      attribute, the 'csrfmiddlewaretoken' the we retrieve from the form's hidden input, and we create a new FormData
+      obj from the Form's content. The data received back is converted to JSON Format and returned.*/
     const result = await fetch(url, {method:method, headers:{'X-CSRFToken':csrfmiddlewaretoken}, body:formData})
     const data = await result.json()
     return data
 }
 
-// Form
+
+// #############################################Event Listeners#########################################################
+
+
+// Form Submit Event Listener
 form.addEventListener('submit', (e) => {
+    /*This event listener will be fired every time as submit is performed, the event will be cancelled, before submitting
+    any content, we first need to check if there are any left blank inputs, if that is the case, we should warn the user
+    that the patient's instance information he is creating is not full, but that he can come back to update it any time,
+    we show up a modal asking if he will continue filling the form or he will continue later, if he decides to continue
+    filling, then the modal will be closed, if not, the form will be submitted.*/
     e.preventDefault()
     e.stopPropagation()
     unfilledInputs = 0
@@ -45,40 +75,53 @@ form.addEventListener('submit', (e) => {
 })
 
 
-// Button
+// Button Event Listeners
 for (let i = 0; i<button.length; i++){
+    /* This event will be fired every time a mouse over occurs over a button, the button-hover class will be added.*/
     button[i].addEventListener('mouseover', function(){
         this.classList.add('button-hover')
     })
 
+    /* This event will be fired every time a mouse out occurs of a button, the button-hover class will be removed.*/
     button[i].addEventListener('mouseout', function(){
         this.classList.remove('button-hover')
     })
 }
 
-
+// Extra Info Event Listeners
 if (extraInfo){
+
+    // Mouse Over events
     extraInfo.addEventListener('mouseover', (e) =>{
+        /* This event will be fired every time a mouse over occurs over a fa-plus icon, the fa-plus-hover class will be added.*/
         if (e.target.classList.contains('fa-plus')){
             e.target.classList.add('fa-plus-hover')
         }
 
+        /* This event will be fired every time a mouse over occurs over a fa-trash icon, the fa-trash-hover class will be added.*/
         if (e.target.classList.contains('fa-trash')){
             e.target.classList.add('fa-trash-hover')
         }
     })
 
     extraInfo.addEventListener('mouseout', (e) =>{
+        /* This event will be fired every time a mouse out occurs of fa-plus icon, the fa-plus-hover class will be removed.*/
         if (e.target.classList.contains('fa-plus')){
             e.target.classList.remove('fa-plus-hover')
         }
 
+        /* This event will be fired every time a mouse out occurs of fa-trash icon, the fa-trash-hover class will be removed.*/
         if (e.target.classList.contains('fa-trash')){
             e.target.classList.remove('fa-trash-hover')
         }
     })
 
+    // Click Events
     extraInfo.addEventListener('click', (e) =>{
+        /*This event will be fired every time a target will the add-allergy class is clicked, this event will perform
+          the following: will extract the 'url' from the data-url attribute, will define the allergySelection variable
+          to the current elements inside the select of the allergiesInformation form, the data received will be displayed
+          inside the modal, it will display a form for insurance addition.*/
         if (e.target.classList.contains('add-allergy')){
             const url = e.target.getAttribute('data-url')
             allergySelection = e.target.parentNode.firstChild
@@ -90,6 +133,9 @@ if (extraInfo){
         }
 
         if (e.target.classList.contains('add-insurance')){
+            /*This event will be fired every time a target will the add-insurance class is clicked, this event will perform
+              the following: will extract the 'url' from the data-url attribute, to make the GET request,the data received
+              will be displayed inside the modal, it will display a form for insurance addition.*/
             const url = e.target.getAttribute('data-url')
             elementAdditionFormAsync(url)
             .then(data => {
@@ -99,6 +145,15 @@ if (extraInfo){
         }
 
         if (e.target.classList.contains('add-allergy-form')){
+            /*This event will be fired every time the target contains the add-allergy-form, this event will add another
+              form to the list of forms available to add as many instances as needed in the Allergies and Antecedents
+              form's list, to create as many instances as the user needs, the event does the following:
+              1. Grab the amount of forms in existence.
+              2. Grab the hidden input #id_form-TOTAL_FORMS for further modifications.
+              3. Clone an allergies_form so we can added to our form's list and change it's attribute values.
+              4. Grab the cloned node and through a loop change all it's attributes depending on the input type.
+              5. Change the Total Forms Value to the amount of forms available plus one unit.
+              6. Append the cloned node to the allergies_form list.*/
             let formAmount = document.querySelectorAll('.allergies-form .form-container')
             let allergyTotalForms = document.querySelector('.allergies_management_form #id_form-TOTAL_FORMS')
             let clonedNode = allergiesFormBlueprint.cloneNode(true)
@@ -121,6 +176,10 @@ if (extraInfo){
         }
 
         if (e.target.classList.contains('delete-allergy-form')){
+            /* This event will be fired every time the target contains the delete-allergy-form class in its classlist,
+               The event will grab the form container of that particular form, it will check the checkbox to perform
+               deletion of that specific form in the backend, and will add the class form-hide to the form container,
+               so that it would disappear from the form's list.*/
             let formContainer = e.target.parentNode.parentNode
             for (let i = 0; i<formContainer.childNodes.length; i++){
                 if (formContainer.childNodes[i].firstChild){
@@ -133,6 +192,15 @@ if (extraInfo){
         }
 
         if (e.target.classList.contains('add-antecedent-form')){
+           /* This event will be fired every time the target contains the add-antecedent-form, this event will add another
+              form to the list of forms available to add as many instances as needed in the Allergies and Antecedents
+              form's list, to create as many instances as the user needs, the event does the following:
+              1. Grab the amount of forms in existence.
+              2. Grab the hidden input #id_form-TOTAL_FORMS for further modifications.
+              3. Clone an antecedents_form so we can added to our form's list and change it's attribute values.
+              4. Grab the cloned node and through a loop change all it's attributes depending on the input type.
+              5. Change the Total Forms Value to the amount of forms available plus one unit.
+              6. Append the cloned node to the antecedents_form list.*/
             let formAmount = document.querySelectorAll('.antecedents-form .form-container')
             let antecedentsTotalForms = document.querySelector('.antecedents_management_form #id_form-TOTAL_FORMS')
             let clonedNode = antecedentsFormBlueprint.cloneNode(true)
@@ -155,6 +223,10 @@ if (extraInfo){
         }
 
         if (e.target.classList.contains('delete-antecedent-form')){
+            /* This event will be fired every time the target contains the delete-antecedent-form class in its classlist,
+               The event will grab the form container of that particular form, it will check the checkbox to perform
+               deletion of that specific form in the backend, and will add the class form-hide to the form container,
+               so that it would disappear from the form's list.*/
             let formContainer = e.target.parentNode.parentNode
             for (let i = 0; i<formContainer.childNodes.length; i++){
                 if (formContainer.childNodes[i].firstChild){
@@ -169,29 +241,36 @@ if (extraInfo){
     })
 }
 
-
+// Modal Event Listeners
 if (modal){
+    // Click Events
+
     modal.addEventListener('click', (e) => {
+        /*This event will be fired every time the target is the modal itself, it will remove the modal-show class.*/
         if (e.target.classList.contains('modal')){
             e.target.classList.remove('modal-show')
         }
     })
 
     modal.addEventListener('mouseover', (e) => {
+        /* This event will be fired every time a mouse over occurs over a button, the button-hover class will be added.*/
         if (e.target.nodeName === 'BUTTON'){
             e.target.classList.add('button-hover')
         }
 
+        /* This event will be fired every time a mouse over occurs over an input, the input-hover class will be added.*/
         if (e.target.nodeName === 'INPUT'){
             e.target.classList.add('input-hover')
         }
     })
 
     modal.addEventListener('mouseout', (e) => {
+        /* This event will be fired every time a mouse out occurs of a button, the button-hover class will be removed.*/
         if (e.target.nodeName === 'BUTTON'){
             e.target.classList.remove('button-hover')
         }
 
+        /* This event will be fired every time a mouse out occurs over a input, the input-hover class will be removed.*/
         if (e.target.nodeName === 'INPUT'){
             e.target.classList.remove('input-hover')
         }
@@ -199,6 +278,11 @@ if (modal){
     })
 
     modal.addEventListener('submit', (e) => {
+        /*This event listener will be fired every time as submit is performed, the event will be cancelled, before submitting
+        any content, we grab some data from the target as it is, the 'url' to where the POST request will be done, the
+        'method' we grab from the method attribute in the target, the 'csrfmiddlewaretoken' we grab from the hidden input
+        inside the form, and we create a new FormData from the data inside the form, if the target contains the 'add-allergy'
+        class, then the select list of the available allergies will be updated, else the insurance selection will be updated.*/
         e.preventDefault()
         e.stopPropagation()
         const form = e.target
@@ -209,7 +293,6 @@ if (modal){
         if (e.target.id = 'add-allergy'){
             addElementAsync(url, method, csrfmiddlewaretoken, formData)
             .then(data => {
-                console.log(data)
                 if (data['html']){
                     modalContent.innerHTML = data['html']
                 }else{
@@ -233,14 +316,16 @@ if (modal){
     })
 }
 
-// Confirmation Modal
-
+// Confirmation Modal Event Listeners
 if (saveConfirmationModal){
+    // Click Events
     saveConfirmationModal.addEventListener('click', (e) => {
+        /*This event will be fired every time the target is the modal itself or a button with "No" in its text content.*/
         if (e.target === saveConfirmationModal || (e.target.nodeName === 'BUTTON' && e.target.textContent === 'No')){
             saveConfirmationModal.classList.remove('save-confirmation-modal-show')
         }
 
+        /*This event will be fired every time the target is button with "Yes" in its text content.*/
         if (e.target.nodeName === 'BUTTON' && e.target.textContent === 'Yes'){
             form.submit()
         }
